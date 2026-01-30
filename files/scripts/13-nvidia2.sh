46990524a42a1d9f92f3e1a78773a6e2c862557d
moneroexamples <marcinwol@gmail.com>
2026-01-30 10:14:56 +0000

    Update 13-nvidia2.sh

--- files/scripts/13-nvidia2.sh
+++ files/scripts/13-nvidia2.sh
@@ -4,4 +4,4 @@ set -ouex pipefail
 
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-dnf install -y akmod-nvidia
+dnf install -y xorg-x11-drv-nvidia
