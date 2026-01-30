7186cffe1d9982e97beb8e5636d715a6cc6d025e
moneroexamples <marcinwol@gmail.com>
2026-01-30 08:49:46 +0000

    Create nvidia2.sh

---
+++ files/scripts/nvidia2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install -y akmod-nvidia
