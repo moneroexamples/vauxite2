799afee4588f8850ba72300e1b043e30d99e850f
moneroexamples <marcinwol@gmail.com>
2026-02-05 08:15:49 +0000

    Update 99-cleanup.sh

--- files/scripts/99-cleanup.sh
+++ files/scripts/99-cleanup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -ouex pipefail
 
-find /tmp -mindepth 1 -maxdepth 1 -exec rm -rf {} + 2>/dev/null || true
-find /var/tmp -mindepth 1 -maxdepth 1 -exec rm -rf {} + 2>/dev/null || true
+find /tmp -mindepth 1 -maxdepth 1 -exec /usr/bin/rm -rf {} + 2>/dev/null || true
+find /var/tmp -mindepth 1 -maxdepth 1 -exec /usr/bin/rm -rf {} + 2>/dev/null || true
 
-rm -rf /var/cache/dnf/* 2>/dev/null || true
-rm -rf /var/cache/dnf5/* 2>/dev/null || true
-rm -rf /var/lib/dnf/* 2>/dev/null || true
+/usr/bin/rm -rf /var/cache/dnf/* 2>/dev/null || true
+/usr/bin/rm -rf /var/cache/dnf5/* 2>/dev/null || true
+/usr/bin/rm -rf /var/lib/dnf/* 2>/dev/null || true
 
-rm -rf /run/* 2>/dev/null || true
+/usr/bin/rm -rf /run/* 2>/dev/null || true
 
 # Clean up Ruby gem cache
-rm -rf /root/.gem 2>/dev/null || true
-rm -rf /tmp/gem* 2>/dev/null || true
+/usr/bin/rm -rf /root/.gem 2>/dev/null || true
+/usr/bin/rm -rf /tmp/gem* 2>/dev/null || true
 
 # The Anaconda installer in derived images complains about stray files in / that
 # shouldn't be there. I have no idea if these files are coming from this image but try
 # to delete them anyway.
 cd /
-rm -f nvim.root 2>/dev/null || true
-rm -f dnf 2>/dev/null || true
+/usr/bin/rm -f nvim.root 2>/dev/null || true
+/usr/bin/rm -f dnf 2>/dev/null || true
 #rm -rf selinux-policy 2>/dev/null || true
 
 #for file in .wget-hsts* .wget-hpkp* .wh.* .*_lck_*; do
