64fb9c649c1d59b76b6de56f3f98bd5beeafb776
moneroexamples <marcinwol@gmail.com>
2026-02-05 22:12:37 +0000

    Update 99-cleanup.sh

--- files/scripts/99-cleanup.sh
+++ files/scripts/99-cleanup.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 set -ouex pipefail
 
-find /tmp -mindepth 1 -maxdepth 1 -exec /usr/bin/rm -rf {} + 2>/dev/null || true
-find /var/tmp -mindepth 1 -maxdepth 1 -exec /usr/bin/rm -rf {} + 2>/dev/null || true
+#find /tmp -mindepth 1 -maxdepth 1 -exec /usr/bin/rm -rf {} + 2>/dev/null || true
+#find /var/tmp -mindepth 1 -maxdepth 1 -exec /usr/bin/rm -rf {} + 2>/dev/null || true
 
-/usr/bin/rm -rf /var/cache/dnf/* 2>/dev/null || true
-/usr/bin/rm -rf /var/cache/dnf5/* 2>/dev/null || true
-/usr/bin/rm -rf /var/lib/dnf/* 2>/dev/null || true
+#/usr/bin/rm -rf /var/cache/dnf/* 2>/dev/null || true
+#/usr/bin/rm -rf /var/cache/dnf5/* 2>/dev/null || true
+#/usr/bin/rm -rf /var/lib/dnf/* 2>/dev/null || true
 
-/usr/bin/rm -rf /run/* 2>/dev/null || true
+#/usr/bin/rm -rf /run/* 2>/dev/null || true
 
 # Clean up Ruby gem cache
-/usr/bin/rm -rf /root/.gem 2>/dev/null || true
-/usr/bin/rm -rf /tmp/gem* 2>/dev/null || true
+#/usr/bin/rm -rf /root/.gem 2>/dev/null || true
+#/usr/bin/rm -rf /tmp/gem* 2>/dev/null || true
 
 # The Anaconda installer in derived images complains about stray files in / that
 # shouldn't be there. I have no idea if these files are coming from this image but try
 # to delete them anyway.
 #cd /
-/usr/bin/rm -f /nvim.root 2>/dev/null || true
-/usr/bin/rm -f /dnf 2>/dev/null || true
+#/usr/bin/rm -f /nvim.root 2>/dev/null || true
+#/usr/bin/rm -f /dnf 2>/dev/null || true
 
 echo "cleanup done"
 
-exit 0
+#exit 0
 
 #/usr/bin/rm -f nvim.root
 #/usr/bin/rm -f dnf
